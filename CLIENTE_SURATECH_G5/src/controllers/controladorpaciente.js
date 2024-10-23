cf03f7f417086686f7d4e7d7280191f47f82dae7
SamuelgiraldoC07 <samuelciro07@gmail.com>
2024-10-23 16:25:57 -0500

    Add files via upload

---
+++ CLIENTE_SURATECH_G5/src/controllers/controladorpaciente.js
@@ -0,0 +1,15 @@
+//OBJETIVO: CAPTURAR Los datos de un formulario 
+
+//1. Por cada input, select, textarea  del formulario se crea una variable
+
+//2. por cada variable debo utilizar el Dom para asociar el html con javascript
+
+let nombrePaciente=document.getElementById("nombrepaciente")
+let correoPaciente=document.getElementById("correopaciente")
+let telefonoPaciente=document.getElementById("telefonopaciente")
+let fechaNacimientoPaciente =document.getElementById("nacimientopaciente")
+let ciudadPaciente=document.getElementById("ciudadpaciente")
+let polizaPaciente=document.getElementById("polizapaciente")
+let ipsPaciente =document.getElementById("ipspaciente")
+let grupoIngresoPaciente=document.getElementById("ingresospaciente")
+let fechaAfiliacionPaciente  =document.getElementById("afeliacionpaciente")
